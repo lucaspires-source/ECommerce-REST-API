419d4ef7faf28509a1465757da93b3c34fada435
lucaspires-source <pires.lucas94@gmail.com>
2022-03-08 14:37:19 -0300

    creating test  routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
@@ -11,6 +12,9 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err) =>{
     console.log(err)
 })
+
+app.use(express.json())
+app.use('/api/user', userRoute)
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Server is running")
