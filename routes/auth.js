05655cd931b13da10235eac251b75fc2b789f684
lucaspires-source <pires.lucas94@gmail.com>
2022-03-11 17:16:31 -0300

    implementing user login

--- routes/auth.js
+++ routes/auth.js
@@ -22,17 +22,19 @@ router.post("/register", async (req,res) =>{
 ///LOGIN
 
 router.post("/login", async (req,res) =>{
-    const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
-    })
     try{
-        const savedUser =   await newUser.save()
-        res.status(201).json(savedUser)
+
+        const user =  await User.findOne({username:req.body.username})
+        !user && res.status(401).json('Wrong Credentials')
+        const hashedPassword = CryptoJS.AES.decrypt(user.password ,process.env.SECRET_KEY)
+
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8)
+        !password && res.status(401).json('Wrong Credentials')
+
+        res.status(200).json(user)
+
     }catch(err){
         res.status(500).json(err)
     }
-
 })
 module.exports = router ;
