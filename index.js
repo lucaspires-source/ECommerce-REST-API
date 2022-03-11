8fa2aca29df726b6b906e6e591d2ea841f6fd0a5
lucaspires-source <pires.lucas94@gmail.com>
2022-03-11 17:04:03 -0300

    implementing user registration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
-
+const authRoute = require('./routes/auth')
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URL)
 })
 
 app.use(express.json())
+
+app.use('/api/auth', authRoute )
 app.use('/api/user', userRoute)
 
 app.listen(process.env.PORT || 5000, () =>{
