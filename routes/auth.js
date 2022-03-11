fba21b47f36ab697c329a2dcbbc9e3dbbb94425f
lucaspires-source <pires.lucas94@gmail.com>
2022-03-11 17:10:11 -0300

    encrypting password

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const router = require("express").Router()
 const User = require('../models/User')
-
+const CryptoJS = require('crypto-js')
 
 //Register
 router.post("/register", async (req,res) =>{
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
     })
     try{
         const savedUser =   await newUser.save()
@@ -18,4 +18,21 @@ router.post("/register", async (req,res) =>{
 
 })
 
+
+///LOGIN
+
+router.post("/login", async (req,res) =>{
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
+    })
+    try{
+        const savedUser =   await newUser.save()
+        res.status(201).json(savedUser)
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
 module.exports = router ;
