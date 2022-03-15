a9d85970b6b3a47f45118a363218bf21a3777d9a
lucaspires-source <pires.lucas94@gmail.com>
2022-03-15 11:08:09 -0300

    VERYFING IF USER IS ADMIN

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_KEY, (err, user) => {
       if (err) res.status(401).json("Token is not Valid");
       req.user = user;
@@ -13,14 +14,28 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const verifyTokenAndAuthorization = (req, res, next) =>{
-  verifyToken(req,res, () =>{
-    if(req.user.id === req.params.id || req.user.isAdmin)  {
-        next()
-    }else {
-        res.status(403).json("You are not alowed to do this action")
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do this action");
     }
-  })
-}
+  });
+};
 
-module.exports = { verifyToken, verifyTokenAndAuthorization};
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do this action");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
